d02ebe8f1327c06eeade4eb383c04f7e692e6595
Sepp <34765@ma-web.nl>
2023-12-05 11:31:40 +0100

    Special attack finished without anim

--- BO-Hades/Assets/Scripts/Player/Attacks/SpecialAttack.cs
+++ BO-Hades/Assets/Scripts/Player/Attacks/SpecialAttack.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 
 public class SpecialAttack : MonoBehaviour
 {
+    public Movement movement;
     public GameObject special;
     public Transform specialpoint;
-    private float cooldown = 30f;
+    public AudioSource SFX;
+    private float cooldown = 1.9f;
     bool canSpecial = true;
-
+    
+    
+    
     internal IEnumerator cd()
     {
-        canSpecial = false;
+        
         yield return new WaitForSeconds(cooldown);
-        canSpecial = true;
+        
 
+    }
+    internal IEnumerator attack()
+    {
+        SFX.Play();
+        movement.enabled = false;
+        yield return new WaitForSeconds(1f);
+        Special();
+        canSpecial = true;
+        movement.enabled = true;
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q) && canSpecial == true)
         {
-           GameObject specialInstance = Instantiate(special, specialpoint);
-            StartCoroutine(cd());
-           Destroy(specialInstance, 0.2f);
+            canSpecial = false;
+            StartCoroutine(attack());
+            
         }
+    }
+    void Special()
+    {
+        GameObject specialInstance = Instantiate(special, specialpoint);
+        StartCoroutine(cd());
+        Destroy(specialInstance, 0.2f);
+        
     }
 }
