8a603011768e72ac2e009f30ce81b04473ad7e22
Clement Malaka <114576100+clem456@users.noreply.github.com>
2023-12-06 10:35:43 +0100

    Added attack scripts

---
+++ BO-Hades/Assets/Scripts/State.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    private static Dictionary<Transform, List<string>> animations = new();
+    private static Dictionary<Transform, string> state = new();
+
+    internal Animator animator;
+
+    internal bool boolean = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        var animatorController = animator.runtimeAnimatorController as UnityEditor.Animations.AnimatorController;
+
+        foreach (var layer in animatorController.layers)
+        {
+            animations.Add(transform, new(layer.stateMachine.states.Length));
+
+            foreach (var animationState in layer.stateMachine.states)
+            {
+                string state = animationState.state.name;
+
+                if (Regex.IsMatch(state, "Attack"))
+                {
+                    int index = int.Parse(Regex.Match(animationState.state.name, @"\d+").Value) - 1;
+
+                    new Attack(state, index, transform, this);
+                }
+
+                animations[transform].Add(state);
+            }
+        }
+
+        state.Add(transform, "Idle");
+    }
+
+    internal void ChangeState(Transform character, string newState)
+    {
+        if (state[character] == newState) { return; }
+
+        bool inAnimations = false;
+
+        foreach (string checkState in animations[character])
+        {
+            if (checkState == newState)
+            {
+                inAnimations = true;
+
+                break;
+            }
+        }
+
+        if (!inAnimations)
+        {
+            Debug.LogWarning($"Cannot find '{newState}' in animation layers");
+        }
+        else
+        {
+            state[character] = newState;
+
+            PlayAnimation(character);
+        }
+    }
+
+    internal string GetState()
+    {
+        return state[transform];
+    }
+
+    private void PlayAnimation(Transform character)
+    {
+        animator.CrossFade(state[character], .2f);
+    }
+}
