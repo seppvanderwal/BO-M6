0baf4197c8fc512ed487679da6c592545c047946
Sepp <34765@ma-web.nl>
2023-12-07 10:51:57 +0100

    test

---
+++ BO-Hades/Assets/Scripts/Environment/PotBreak.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PotBreak : MonoBehaviour
+{
+    public Rigidbody test;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("test");
+    }
+}
