9de2e6e0bfa9f85730438bc41314348781e1dac9
Sepp <34765@ma-web.nl>
2024-01-11 12:20:18 +0100

    UI placeholders en begin particle system

---
+++ BO-Hades/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int maxhealth = 20;
+    public Slider healthbar;
+    public TextMeshProUGUI health;
+    public static int hp;
+    void Start()
+    {
+        hp = 50;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            hp -= 10;
+        }
+        healthbar.maxValue = maxhealth;
+        healthbar.value = hp;
+        health.text = hp.ToString() + " / " + maxhealth.ToString();
+
+    }
+}
+
