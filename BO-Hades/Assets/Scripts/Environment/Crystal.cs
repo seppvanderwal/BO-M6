0751a2af1201cfaeec7b41b908ccd6cd24df3198
Clement Malaka <114576100+clem456@users.noreply.github.com>
2024-01-16 09:23:26 +0100

    Cast works

---
+++ BO-Hades/Assets/Scripts/Environment/Crystal.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    public float lifetime = 1;
+
+    public float height = .2f;
+    public float speed = 2;
+    public float moveSpeed = 30;
+    public float increment = 0.05f;
+
+    private Vector3 originalPos;
+
+    private bool idle = true;
+
+    private float currentIncrement;
+    private float timer = 0;
+
+    private Transform character;
+
+    private void Start()
+    {
+        originalPos = transform.position;
+
+        currentIncrement = increment;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            character = collider.transform;
+            idle = false;
+
+            State state = character.GetComponent<State>();
+            state.cast.cooldown = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!idle)
+        {
+            if (timer >= lifetime)
+            {
+                Destroy(gameObject);
+            }
+
+            timer += Time.deltaTime;
+
+            transform.localScale -= new Vector3(1.5f, 1.5f, 1.5f) * Time.deltaTime * moveSpeed;
+        }
+        else
+        {
+            if (transform.position.y >= originalPos.y + height)
+            {
+                currentIncrement = -increment;
+            }
+            else if (transform.position.y <= originalPos.y - height)
+            {
+                currentIncrement = increment;
+            }
+
+            transform.position += new Vector3(0, currentIncrement * speed * Time.deltaTime, 0);
+        }
+
+        /*
+        if (Vector3.Distance(character.position, originalPos) <= distance)
+        {
+            idle = false;
+        }*/
+    }
+}
--- BO-Hades/Assets/Scripts/Player/Attacks/Gun.cs
+++ BO-Hades/Assets/Scripts/Player/Attacks/Gun.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
@@ -11,13 +9,13 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E)) 
+        if (Input.GetKeyDown(KeyCode.E))
         {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
