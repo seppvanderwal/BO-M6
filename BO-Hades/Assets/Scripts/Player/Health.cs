c47eb5bc2de17ea991d3cada43d7e494c9809fe6
Sepp <34765@ma-web.nl>
2024-01-16 13:02:24 +0100

    ui en death

--- BO-Hades/Assets/Scripts/Player/Health.cs
+++ BO-Hades/Assets/Scripts/Player/Health.cs
@@ -1,15 +1,18 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    public string scene;
     public int maxhealth = 20;
     public Slider healthbar;
     public TextMeshProUGUI health;
     public static int hp;
     void Start()
     {
+
         hp = 50;
     }
     private void Update()
@@ -21,7 +24,10 @@ public class Health : MonoBehaviour
         healthbar.maxValue = maxhealth;
         healthbar.value = hp;
         health.text = hp.ToString() + " / " + maxhealth.ToString();
-
+        if (hp == 0)
+        {
+            SceneManager.LoadScene(sceneName: SceneManager.GetActiveScene().name);
+        }
     }
 }
 
