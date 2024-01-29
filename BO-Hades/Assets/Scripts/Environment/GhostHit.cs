3698d8fbf5be7f95b2525059b7149489e12f3a22
Dylan000000 <132989962+Dylan000000@users.noreply.github.com>
2024-01-29 22:02:08 +0100

    Merge branch 'develop' into Feature_Blockout_Dylan

--- BO-Hades/Assets/Scripts/Environment/GhostHit.cs
+++ BO-Hades/Assets/Scripts/Environment/GhostHit.cs
@@ -5,13 +5,11 @@ public class GhostHit : MonoBehaviour
 {
     public ParticleSystem Particle;
     public bool hit = false;
-    private void Start()
-    {
 
-    }
     internal IEnumerator Die()
     {
         gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
         Particle.Play();
         yield return new WaitForSeconds(1f);
         Particle.Stop();
